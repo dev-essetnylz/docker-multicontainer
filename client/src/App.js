f37577185aef650dafe2a75c829e350292e9a94c
Raghav-31-k <essentylz.app@gmail.com>
2025-02-03 20:10:55 +0530

    EBS update

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React s
           </a>
           <Link to="/">Home Ok 1</Link>
           <Link to="/otherpage">Other Page</Link>
